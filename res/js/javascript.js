849924ebaf49efa11e8afca62b231f9a54b5974d
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2023-11-14 18:52:41 +0100

    Added combos, animations, restart button and sound

--- res/js/javascript.js
+++ res/js/javascript.js
@@ -5,6 +5,8 @@ const spinace = document.getElementById("spin");
 const slot1 = document.getElementById("1-slot");
 const slot2 = document.getElementById("2-slot");
 const slot3 = document.getElementById("3-slot");
+const restart = document.getElementById("restart");
+var sound = new Audio('res/sound/bigwin.mp3');
 
 let lovaky = 1000;
 let decreaseLove = 100;
@@ -19,17 +21,14 @@ spinace.onclick = () => {
     var randomNum3 = Math.floor(Math.random() * icons.length);
     slot3.src = icons[randomNum3];
 
-    if (lovaky <= 0) {
-        spinace.style.pointerEvents = "none";
-        spinace.style.filter = "saturate(0)";
-        counter.innerText = "exekuce GG";
-    }
-
     if (
         slot1.src.endsWith("res/img/tresen.png") &&
         slot2.src.endsWith("res/img/tresen.png") &&
         slot3.src.endsWith("res/img/tresen.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 3000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -38,6 +37,9 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/hrozny.png") &&
         slot3.src.endsWith("res/img/hrozny.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 3000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -46,6 +48,9 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/listek.png") &&
         slot3.src.endsWith("res/img/listek.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 5000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -54,6 +59,9 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/sedmicka.png") &&
         slot3.src.endsWith("res/img/sedmicka.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 4000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -62,6 +70,9 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/diamant.png") &&
         slot3.src.endsWith("res/img/diamant.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 4000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -70,6 +81,9 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/bar.png") &&
         slot3.src.endsWith("res/img/bar.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 2000;
         counter.innerHTML = "Love: " + lovaky;
     }
@@ -78,12 +92,63 @@ spinace.onclick = () => {
         slot2.src.endsWith("res/img/dolar.png") &&
         slot3.src.endsWith("res/img/dolar.png")) {
         bigWin();
+        animSlot(slot1);
+        animSlot(slot2);
+        animSlot(slot3);
         lovaky += 2000;
         counter.innerHTML = "Love: " + lovaky;
     }
+    /* Combo winy */
+    if (
+        slot1.src.endsWith("res/img/diamant.png") &&
+        slot3.src.endsWith("res/img/diamant.png") &&
+        !slot2.src.endsWith("res/img/diamant.png")) {
+        comboWin();
+        animSlot(slot1);
+        animSlot(slot3);
+        lovaky += 1000;
+        counter.innerHTML = "Love: " + lovaky;
+    }
+    if (
+        slot1.src.endsWith("res/img/listek.png") &&
+        slot3.src.endsWith("res/img/listek.png") &&
+        !slot2.src.endsWith("res/img/listek.png")) {
+        comboWin();
+        animSlot(slot1);
+        animSlot(slot3);
+        lovaky += 1500;
+        counter.innerHTML = "Love: " + lovaky;
+    }
+    if (
+        slot1.src.endsWith("res/img/bar.png") &&
+        slot3.src.endsWith("res/img/bar.png") &&
+        !slot2.src.endsWith("res/img/bar.png")) {
+        comboWin();
+        animSlot(slot1);
+        animSlot(slot3);
+        lovaky += 1000;
+        counter.innerHTML = "Love: " + lovaky;
+    }
+
+    /* Check na love musi bejt na konci jinak se to dosere */
+    if (lovaky <= 0) {
+        spinace.style.pointerEvents = "none";
+        spinace.style.filter = "saturate(0)";
+        counter.innerText = "exekuce GG";
+        restart.style.display = "inline-block";
+    }
+}
+
+restart.onclick = () => {
+    restart.style.display = "none";
+    lovaky = 1000;
+    counter.innerHTML = "Love: " + lovaky;
+    spinace.style.pointerEvents = "all";
+    spinace.style.filter = "saturate(1)";
 }
 
 function bigWin() {
+    sound.play();
     spinace.style.filter = "saturate(1)";
     spinace.style.pointerEvents = "none";
     const bigWinImage = document.createElement("img");
@@ -96,5 +161,42 @@ function bigWin() {
         if (lovaky > 0) {
             spinace.style.pointerEvents = "all";
         }
-    }, 2995);
+    }, 2990);
+}
+
+function comboWin() {
+    spinace.style.filter = "saturate(1)";
+    spinace.style.pointerEvents = "none";
+    const comboWinImage = document.createElement("img");
+    comboWinImage.src = "res/img/combo.png";
+    comboWinImage.id = "combowin";
+    comboWinImage.draggable = false;
+    document.body.appendChild(comboWinImage);
+    setTimeout(function () {
+        comboWinImage.parentNode.removeChild(comboWinImage);
+        if (lovaky > 0) {
+            spinace.style.pointerEvents = "all";
+        }
+    }, 990);
+}
+
+function animSlot(slot) {
+    if (slot === slot1) {
+        slot1.style.transform = "scale(1.35)";
+        setTimeout(function () {
+            slot1.style.transform = "scale(1)";
+        }, 1000);
+    } else if (slot === slot2) {
+        slot2.style.transform = "scale(1.35)";
+        setTimeout(function () {
+            slot2.style.transform = "scale(1)";
+        }, 1000);
+    } else if (slot === slot3) {
+        slot3.style.transform = "scale(1.35)";
+        setTimeout(function () {
+            slot3.style.transform = "scale(1)";
+        }, 1000);
+    } else {
+        console.log("zadnej takovej slot neni");
+    }
 }
